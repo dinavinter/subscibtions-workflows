9fa63af9ed394a3195741d0bb53bb48bc2576177
din <dina.vinter@sap.com>
2021-10-06 11:46:58 +0300

    scripts

---
+++ src/AspNetCoreDemoApp/Scripts/plugins/OutcomesDriver.js
@@ -0,0 +1,27 @@
+
+
+export function  DriversPlugin (elsaStudio) {
+  const {propertyDisplayManager, getOrCreateProperty} =  elsaStudio;
+  propertyDisplayManager.addDriver('outcomes', (elsaStudio) => elsaStudio.propertyDisplayManager.getDriver('multi-text'));
+
+}
+
+
+
+
+// export class MultiTextDriver implements PropertyDisplayDriver {
+//     private readonly elsaStudio: ElsaStudio;
+//
+//     constructor(elsaStudio: ElsaStudio) {
+//        this.elsaStudio =elsaStudio;
+//      }
+//
+//   display(activity: ActivityModel, property: ActivityPropertyDescriptor) {
+//       const {propertyDisplayManager, getOrCreateProperty} =  this.elsaStudio;
+//       const prop =  getOrCreateProperty(activity, property.name);
+//     return <elsa-multi-text-property propertyDescriptor={property} propertyModel={prop}/>;
+//   }
+//
+//   update(activity: ActivityModel, property: ActivityPropertyDescriptor, form: FormData) {
+//   }
+// }
---
+++ src/AspNetCoreDemoApp/Scripts/plugins/plugins.js
@@ -0,0 +1,23 @@
+// import {parseJson} from "./utils";
+
+import {EventReceivedPlugin} from "./EventReceived.js";
+import {DriversPlugin} from "./OutcomesDriver.js";
+
+const elsaStudioRoot = document.querySelector('elsa-studio-root');
+
+elsaStudioRoot.addEventListener('initializing', e => {
+  const elsaStudio = e.detail;
+
+  import {EventReceivedPlugin} from "./EventReceived.js";
+  import {DriversPlugin} from "./OutcomesDriver.js";
+
+  const elsaStudioRoot = document.querySelector('elsa-studio-root');
+
+  elsaStudioRoot.addEventListener('initializing', e => {
+    const elsaStudio = e.detail;
+
+    elsaStudio.pluginManager.registerPlugins([EventReceivedPlugin, DriversPlugin]);
+  });
+
+});
+
